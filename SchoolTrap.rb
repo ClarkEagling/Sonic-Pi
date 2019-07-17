31e7b84dd3a62579ad117801839ff257e9593866
Clark Eagling - ee33 <37630289+ClarkEagling@users.noreply.github.com>
2019-07-17 16:10:17 -0400

    Add files via upload

---
+++ SchoolTrap.rb
@@ -0,0 +1,174 @@
+use_bpm 145
+
+
+#Temp Notes
+def sad_lfo
+  (ring 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8).tick
+end
+
+live_loop :arp do
+  play (scale :e3, :minor_pentatonic).tick, amp: rrand(0,0.7), release: rrand(0.1,1)
+  play (scale :e3, :minor_pentatonic).reverse, amp: rrand(0.1,1), release: rrand(0.1,1)
+  sleep 0.5
+end
+
+#BASS
+#BASS
+#BASS
+
+define :b0 do
+  use_synth :tri
+  sleep 4
+  play 28, amp: 3, attack: 0, sustain: 0.4, release: 1
+  sleep 4
+end
+
+define :b1 do
+  use_synth :tri
+  sleep 4
+  play 24, amp: 3, attack: 0, sustain: 0.4, release: 1
+  sleep 4
+end
+
+live_loop :bass1 do
+  4.times do
+    b0
+  end
+  4.times do
+    b1
+  end
+end
+
+
+#KICKS
+#KICKS
+#KICKS
+def kick1(v)
+  sample :bd_haus, amp: v, rate: 1.0
+end
+def kick2(v)
+  sample :bd_mehackit, amp: v, rate: 2.0
+end
+
+live_loop :kicks1 do
+  kick1(2)
+  sleep 3
+  kick1(2)
+  sleep 5
+end
+
+live_loop :kicks2 do
+  k = [0,0,1].choose
+  case k
+  when 0
+    8.times do
+      sleep 1
+    end
+  when 1
+    1.times do
+      sleep 0.5
+      kick1(1.5)
+      sleep 1.5
+    end
+  end
+end
+
+#SNARES
+#SNARES
+#SNARES
+def snar(v)
+  sample :drum_snare_hard, rate: 2, amp: v, attack: 0, decay: 0.3, sustain: 0, release: 0
+end
+
+define :s0 do
+  2.times do
+    sleep 2
+    snar(2)
+    sleep 2
+  end
+end
+
+define :s1 do
+  1.times do
+    sleep 2
+    snar(2)
+    sleep 1.5
+    snar(1.5)
+    sleep 2.5
+    snar(2)
+    sleep 2
+  end
+end
+
+define :s2 do
+  1.times do
+    sleep 2
+    snar(2)
+    sleep 1.5
+    snar(1.5)
+    sleep 2.5
+    snar(2)
+    sleep 0.5
+    snar(1)
+    sleep 1.5
+  end
+end
+
+define :s3 do
+  1.times do
+    sleep 2
+    snar(2)
+    sleep 1.5
+    snar(1.5)
+    sleep 1.5
+    snar(1.2)
+    sleep 0.5
+    snar(1.5)
+    sleep 2.5
+  end
+end
+
+live_loop :snares1 do
+  play [s0,s0,s1,s2,s0,s0,s1,s3]
+end
+
+#HI HATS
+#HI HATS
+#HI HATS
+def hat(v,d,r)
+  sample :drum_cymbal_closed, rate: r, amp: v, attack: 0, decay: d, sustain: 0, release: 0
+end
+
+live_loop :hats do
+  trips = [0,0,1,2,3].choose
+  case trips
+  when 0
+    8.times do
+      hat(1.4,0.04,1)
+      sleep 0.5
+    end
+  when 1
+    4.times do
+      hat(1.2,0.02,1)
+      sleep 0.25
+      hat(1.3,0.01,1)
+      sleep 0.25
+    end
+  when 2
+    4.times do
+      hat(1,0.01,rrand(0,1))
+      sleep 0.125
+      hat(0.8,0.02,rrand(0,1))
+      sleep 0.125
+    end
+  when 3
+    2.times do
+      hat(1.3,0.03,1)
+      sleep 0.32
+      hat(1.2,0.01,1)
+      sleep 0.32
+      hat(1.2,0.02,1)
+      sleep 0.36
+    end
+  end
+end
