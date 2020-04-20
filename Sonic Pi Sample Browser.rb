812619d0f670d3549ee76ac39b6544fd1e76c109
Clark Eagling - ee33 <37630289+ClarkEagling@users.noreply.github.com>
2020-04-19 22:38:35 -0400

    Add files via upload

--- Sonic Pi Sample Browser.rb
+++ Sonic Pi Sample Browser.rb
@@ -1,184 +1,185 @@
+rat = 3
+sus = 0.01
 use_bpm 55
-sample :drum_heavy_kick
+sample :drum_heavy_kick, rate: rat, sustain: sus
 sleep 1
-sample :drum_tom_mid_soft
+sample :drum_tom_mid_soft, rate: rat, sustain: sus
 sleep 1
-sample :drum_tom_mid_hard
+sample :drum_tom_mid_hard, rate: rat, sustain: sus
 sleep 1
-sample :drum_tom_lo_soft
+sample :drum_tom_lo_soft, rate: rat, sustain: sus
 sleep 1
-sample :drum_tom_lo_hard
+sample :drum_tom_lo_hard, rate: rat, sustain: sus
 sleep 1
-sample :drum_tom_hi_soft
+sample :drum_tom_hi_soft, rate: rat, sustain: sus
 sleep 1
-sample :drum_tom_hi_hard
+sample :drum_tom_hi_hard, rate: rat, sustain: sus
 sleep 1
-sample :drum_splash_soft
+sample :drum_splash_soft, rate: rat, sustain: sus
 sleep 1
-sample :drum_splash_hard
+sample :drum_splash_hard, rate: rat, sustain: sus
 sleep 1
-sample :drum_snare_soft
+sample :drum_snare_soft, rate: rat, sustain: sus
 sleep 1
-sample :drum_snare_hard
+sample :drum_snare_hard, rate: rat, sustain: sus
 sleep 1
-sample :drum_cymbal_soft
+sample :drum_cymbal_soft, rate: rat, sustain: sus
 sleep 1
-sample :drum_cymbal_hard
+sample :drum_cymbal_hard, rate: rat, sustain: sus
 sleep 1
-sample :drum_cymbal_open
+sample :drum_cymbal_open, rate: rat, sustain: sus
 sleep 1
-sample :drum_cymbal_closed
+sample :drum_cymbal_closed, rate: rat, sustain: sus
 sleep 1
-sample :drum_cymbal_pedal
+sample :drum_cymbal_pedal, rate: rat, sustain: sus
 sleep 1
-sample :drum_bass_soft
+sample :drum_bass_soft, rate: rat, sustain: sus
 sleep 1
-sample :drum_bass_hard
+sample :drum_bass_hard, rate: rat, sustain: sus
 sleep 1
-sample :elec_triangle
+sample :elec_triangle, rate: rat, sustain: sus
 sleep 1
-sample :elec_snare
+sample :elec_snare, rate: rat, sustain: sus
 sleep 1
-sample :elec_lo_snare
+sample :elec_lo_snare, rate: rat, sustain: sus
 sleep 1
-sample :elec_hi_snare
+sample :elec_hi_snare, rate: rat, sustain: sus
 sleep 1
-sample :elec_mid_snare
+sample :elec_mid_snare, rate: rat, sustain: sus
 sleep 1
-sample :elec_cymbal
+sample :elec_cymbal, rate: rat, sustain: sus
 sleep 1
-sample :elec_soft_kick
+sample :elec_soft_kick, rate: rat, sustain: sus
 sleep 1
-sample :elec_filt_snare
+sample :elec_filt_snare, rate: rat, sustain: sus
 sleep 1
-sample :elec_fuzz_tom
+sample :elec_fuzz_tom, rate: rat, sustain: sus
 sleep 1
-sample :elec_chime
+sample :elec_chime, rate: rat, sustain: sus
 sleep 1
-sample :elec_bong
+sample :elec_bong, rate: rat, sustain: sus
 sleep 1
-sample :elec_twang
+sample :elec_twang, rate: rat, sustain: sus
 sleep 1
-sample :elec_wood
+sample :elec_wood, rate: rat, sustain: sus
 sleep 1
-sample :elec_pop
+sample :elec_pop, rate: rat, sustain: sus
 sleep 1
-sample :elec_beep
+sample :elec_beep, rate: rat, sustain: sus
 sleep 1
-sample :elec_blip
+sample :elec_blip, rate: rat, sustain: sus
 sleep 1
-sample :elec_blip2
+sample :elec_blip2, rate: rat, sustain: sus
 sleep 1
-sample :elec_ping
+sample :elec_ping, rate: rat, sustain: sus
 sleep 1
-sample :elec_bell
+sample :elec_bell, rate: rat, sustain: sus
 sleep 1
-sample :elec_flip
+sample :elec_flip, rate: rat, sustain: sus
 sleep 1
-sample :elec_tick
+sample :elec_tick, rate: rat, sustain: sus
 sleep 1
-sample :elec_hollow_kick
+sample :elec_hollow_kick, rate: rat, sustain: sus
 sleep 1
-sample :elec_twip
+sample :elec_twip, rate: rat, sustain: sus
 sleep 1
-sample :elec_plip
+sample :elec_plip, rate: rat, sustain: sus
 sleep 1
-sample :elec_blup
+sample :elec_blup, rate: rat, sustain: sus
 sleep 1
-sample :guit_harmonics
+sample :guit_harmonics, rate: rat, sustain: sus
 sleep 1
-sample :guit_e_fifths
+sample :guit_e_fifths, rate: rat, sustain: sus
 sleep 1
-sample :guit_e_slide
+sample :guit_e_slide, rate: rat, sustain: sus
 sleep 1
-sample :guit_em9
+sample :guit_em9, rate: rat, sustain: sus
 sleep 1
-sample :misc_burp
+sample :misc_burp, rate: rat, sustain: sus
 sleep 1
-sample :perc_bell
+sample :perc_bell, rate: rat, sustain: sus
 sleep 1
-sample :perc_snap
+sample :perc_snap, rate: rat, sustain: sus
 sleep 1
-sample :perc_snap2
+sample :perc_snap2, rate: rat, sustain: sus
 sleep 1
-sample :ambi_soft_buzz
+sample :ambi_soft_buzz, rate: rat, sustain: sus
 sleep 1
-sample :ambi_swoosh
+sample :ambi_swoosh, rate: rat, sustain: sus
 sleep 1
-sample :ambi_drone
+sample :ambi_drone, rate: rat, sustain: sus
 sleep 1
-sample :ambi_glass_hum
+sample :ambi_glass_hum, rate: rat, sustain: sus
 sleep 1
-sample :ambi_glass_rub
+sample :ambi_glass_rub, rate: rat, sustain: sus
 sleep 1
-sample :ambi_haunted_hum
+sample :ambi_haunted_hum, rate: rat, sustain: sus
 sleep 1
-sample :ambi_piano
+sample :ambi_piano, rate: rat, sustain: sus
 sleep 1
-sample :ambi_lunar_land
+sample :ambi_lunar_land, rate: rat, sustain: sus
 sleep 1
-sample :ambi_dark_woosh
+sample :ambi_dark_woosh, rate: rat, sustain: sus
 sleep 1
-sample :ambi_choir
+sample :ambi_choir, rate: rat, sustain: sus
 sleep 1
-sample :bass_hit_c
+sample :bass_hit_c, rate: rat, sustain: sus
 sleep 1
-sample :bass_hard_c
+sample :bass_hard_c, rate: rat, sustain: sus
 sleep 1
-sample :bass_thick_c
+sample :bass_thick_c, rate: rat, sustain: sus
 sleep 1
-sample :bass_drop_c
+sample :bass_drop_c, rate: rat, sustain: sus
 sleep 1
-sample :bass_woodsy_c
+sample :bass_woodsy_c, rate: rat, sustain: sus
 sleep 1
-sample :bass_voxy_c
+sample :bass_voxy_c, rate: rat, sustain: sus
 sleep 1
-sample :bass_voxy_hit_c
+sample :bass_voxy_hit_c, rate: rat, sustain: sus
 sleep 1
-sample :bass_dnb_f
+sample :bass_dnb_f, rate: rat, sustain: sus
 sleep 1
-sample :sn_dub
+sample :sn_dub, rate: rat, sustain: sus
 sleep 1
-sample :sn_dolf
+sample :sn_dolf, rate: rat, sustain: sus
 sleep 1
-sample :sn_zome
+sample :sn_zome, rate: rat, sustain: sus
 sleep 1
-sample :bd_ada
+sample :bd_ada, rate: rat, sustain: sus
 sleep 1
-sample :bd_pure
+sample :bd_pure, rate: rat, sustain: sus
 sleep 1
-sample :bd_808
+sample :bd_808, rate: rat, sustain: sus
 sleep 1
-sample :bd_zum
+sample :bd_zum, rate: rat, sustain: sus
 sleep 1
-sample :bd_gas
+sample :bd_gas, rate: rat, sustain: sus
 sleep 1
-sample :bd_sone
+sample :bd_sone, rate: rat, sustain: sus
 sleep 1
-sample :bd_haus
+sample :bd_haus, rate: rat, sustain: sus
 sleep 1
-sample :bd_zome
+sample :bd_zome, rate: rat, sustain: sus
 sleep 1
-sample :bd_boom
+sample :bd_boom, rate: rat, sustain: sus
 sleep 1
-sample :bd_klub
+sample :bd_klub, rate: rat, sustain: sus
 sleep 1
-sample :bd_fat
+sample :bd_fat, rate: rat, sustain: sus
 sleep 1
-sample :bd_tek
+sample :bd_tek, rate: rat, sustain: sus
 sleep 1
-sample :loop_industrial
+sample :loop_industrial, rate: rat, sustain: sus
 sleep 1
-sample :loop_compus
+sample :loop_compus, rate: rat, sustain: sus
 sleep 1
-sample :loop_amen
+sample :loop_amen, rate: rat, sustain: sus
 sleep 1
-sample :loop_amen_full
+sample :loop_amen_full, rate: rat, sustain: sus
 sleep 1
-sample :loop_garzul
+sample :loop_garzul, rate: rat, sustain: sus
 sleep 1
-sample :loop_mika
+sample :loop_mika, rate: rat, sustain: sus
 sleep 1
-sample :loop_breakbeat
+sample :loop_breakbeat, rate: rat, sustain: sus
 sleep 1
-
---
+++ Tek Until 8 Bar Bass hits with Drum.rb
@@ -0,0 +1,230 @@
+#Global Variables for instruments and note arrays can be created and accessed with "set" and "get"
+
+use_bpm 400
+use_random_seed 2099
+
+define :snareMaker do
+  a=[0,0.7,1]
+  b=a.choose
+  c=a.choose
+  d=a.choose
+  e=a.choose
+  snareline=[b,c,d,e]
+  
+  
+  2.times do
+    4.times do
+      #print timeFixer
+      x=snareline.tick(:tickd)
+      sleep 4
+      sample :sn_dolf, amp:x, rate: rrand(1.9,2.2)
+      sample :sn_generic, amp:x*0.4, rate: rrand(1.9,2.2)
+      sleep 4
+    end
+  end
+end
+
+live_loop :snarePlayer do
+  snareMaker
+end
+
+define :kickMaker do
+  a=[4,8,2]
+  timeFixer = 0
+  until timeFixer == 32
+    b=a.choose
+    c=a.choose
+    d=a.choose
+    e=a.choose
+    f=a.choose
+    g=a.choose
+    h=a.choose
+    timeFixer=b+c+d+e+f+g+h
+  end
+  kickline=[b,c,d,e,f,g,h]
+  
+  aa = [24,26,28,29,31,33,35,
+        36,38,40,41,43,45,47,
+        48,50,52,53,55]
+  ba=aa.choose
+  ca=aa.choose
+  da=aa.choose
+  ea=aa.choose
+  fa=aa.choose
+  ga=aa.choose
+  ha=aa.choose
+  bassline=[ba,ca,da,ea,fa,ga,ha]
+  
+  
+  4.times do
+    
+    7.times do
+      with_fx :distortion, amp: 0.8 do
+        #print timeFixer
+        xx=bassline.tick(:ticka)
+        rel = rrand(0.7,2.7)
+        use_synth :beep
+        play xx, release: rel
+        use_synth :pulse
+        use_synth_defaults pulse_width: rrand(0.1,1), cutoff: rrand(85,105)
+        play xx, release: rel
+      end
+      
+      x=kickline.tick(:tickc)
+      sample :bd_tek, cutoff: rrand(100,130)
+      sleep x
+    end
+  end
+end
+
+
+live_loop :kickPlayer do
+  kickMaker
+end
+
+
+
+#HIGHER CHORDS
+#chords as variables, then array of variables,then choose 'em
+define :chordMaker do
+  
+  #chord arrays
+  b=[72,77,79]#cfg
+  c=[84,89,92]#cfg2
+  
+  d=[74,76,81] #dea
+  e=[86,88,93]#dea2
+  
+  f=[74,79,81]#dga
+  g=[86,91,93]#dga2
+  
+  h=[72,77,79,83]#cfgb
+  i=[84,89,91,95]#cfgb
+  
+  chordSet=[b,d,f,h,c,e,g,i]
+  
+  #chord riff chooser
+  j=chordSet.choose
+  k=chordSet.choose
+  l=chordSet.choose
+  m=chordSet.choose
+  n=chordSet.choose
+  o=chordSet.choose
+  p=chordSet.choose
+  q=chordSet.choose
+  chordrun=[j,k,l,m,n,o,p,q]
+  
+  #note on or off
+  r=[1,0,0,0,0].choose
+  s=[1,1,0,0,0].choose
+  t=[1,1,0,0,0].choose
+  u=[1,1,0,0,0].choose
+  v=[1,0,0,0,0].choose
+  w=[1,1,0,0,0].choose
+  x=[1,1,0,0,0].choose
+  y=[1,0,0,0,0].choose
+  amperage=[r,s,t,u,v,w,x,y]
+  slp=[0.25,1,2,3].choose
+  
+  16.times do
+    xx=chordrun.tick(:tickd)
+    yy=amperage.tick(:ticke)
+    
+    use_synth :piano
+    use_synth_defaults pan: 0.5, release: 8, cutoff: rrand(80,100)
+    play xx, amp: yy
+    sleep slp
+    play xx, amp: yy
+    use_synth :pluck
+    use_synth_defaults pan: 0.25, release: 8, cutoff: rrand(80,100)
+    play xx, amp: yy
+    sleep 8-slp
+  end
+end
+
+with_fx :hpf, pre_amp:8, cutoff: 100, amp: 0.7 do
+  with_fx :reverb, room:0.8, mix: 0.7 do
+    live_loop :chordPlayer do
+      chordMaker
+    end
+  end
+end
+
+
+with_fx :hpf, cutoff: 120 do
+  with_fx :distortion, distort: 0.8, amp:0.8 do
+    live_loop :chick do
+      sample :elec_hi_snare, pan: -0.5, rate: 3, attack:0.2, amp: [1,1,0].choose
+      sleep 2
+      sample :elec_hi_snare, pan: -0.5, rate: 2.85, attack:0.2, amp: 0.8
+      sleep 2
+    end
+    
+    live_loop :chick2 do
+      sleep 1
+      sample :elec_mid_snare, pan: -0.5, rate: 4, attack:0.2
+      sleep 2
+      sample :elec_mid_snare, pan: -0.5, rate: 3.5, attack:0.2, amp: [1,0].choose
+      sleep 1
+    end
+  end
+end
+
+define :arpMaker do
+  #note chooser
+  a = [60,62,64,65,67,69,71,
+       72,74,76,77,78,81,83]
+  b=a.choose
+  c=a.choose
+  d=a.choose
+  e=a.choose
+  f=a.choose
+  g=a.choose
+  h=a.choose
+  i=a.choose
+  arpline=[b,c,d,e,f,g,h,i]
+  
+  #note on or off
+  
+  2.times do
+    j=[1,1,0,0,0].choose
+    k=[1,1,0,0,0].choose
+    l=[1,1,0,0,0].choose
+    m=[1,1,0,0,0].choose
+    n=[1,1,0,0,0].choose
+    o=[1,1,0,0,0].choose
+    p=[1,1,0,0,0].choose
+    q=[1,1,0,0,0].choose
+    amperage=[j,k,l,m,n,o,p,q]
+    div = [2,2,1.5].choose
+    128.times do
+      x=arpline.tick(:ticka)
+      y=amperage.tick(:tickb)
+      rel = rrand(0.4,2)
+      atk = rrand(0,0.5)
+      use_synth :fm
+      play x+12, amp:y*0.3, pan: rrand(-1,0.5), attack:atk, release: rel, divisor:div
+      use_synth :pulse
+      use_synth_defaults pulse_width: rrand(0.01,1), cutoff: rrand(80,100)
+      play x, amp: y*0.4, pan: rrand(-1,0.5), attack:atk,  release: rel
+      sleep 1
+    end
+  end
+  
+  
+  1.times do
+    sleep 128
+  end
+end
+
+
+with_fx :reverb, room: 0.1, mix: 0.7 do
+  with_fx :distortion, amp: 0.4  do
+    live_loop :arpPlayer, delay: 256 do
+      arpMaker
+    end
+  end
+end
+
+
+#well livestreaming is too much for this ol' computer, sound glitching out, thanks for joining me on this boring journey
